61baa31f560c6c1a3b8c5b813a3d1f8a0ff76c20
Travis <travis@travis-ci.org>
2015-02-14 11:19:43 +0000

    Travis build 104 pushed to gh-pages

---
+++ metrics/files/app_scripts_vendor_geoxml3_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Sat, 14 Feb 2015 11:19:41 GMT","sloc":1182,"lloc":828,"functions":47,"deliveredBugs":18.75348226457118,"maintainability":51.66464772280109,"lintErrors":51,"difficulty":152.5655021834061}]
