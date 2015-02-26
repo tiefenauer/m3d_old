46d9db79d6c1168753de527e91af4d6265acb3b9
Travis <travis@travis-ci.org>
2015-02-26 17:59:38 +0000

    Travis build 112 pushed to gh-pages

---
+++ metrics/files/app_scripts_models_footprint_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Thu, 26 Feb 2015 17:59:35 GMT","sloc":34,"lloc":11,"functions":5,"deliveredBugs":0.10545413751658661,"maintainability":83.41269666962744,"lintErrors":4,"difficulty":8.23529411764706}]
